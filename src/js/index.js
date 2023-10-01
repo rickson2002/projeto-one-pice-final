2a1296df6434369b11961008e682971fd81856c7
Rickson De Souza Ferreira <112441890+rickson2002@users.noreply.github.com>
2023-10-01 12:08:57 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+
+const botoes = document.querySelectorAll(".botao");
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+
+        desselescionarBotao();
+        desselecionarPersonagem();
+
+        botao.classList.add("selecionado");
+        personagens[indice].classList.add("selecionado");
+    });
+});
+
+function desselecionarPersonagem() {
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselescionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
+
